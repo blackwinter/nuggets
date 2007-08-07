04813de25276e4296ec01a380c1dd94968034d64
Jens Wille <jens.wille@uni-koeln.de>
2007-08-07 14:44:40 +0000

    Added parameter to handle exceptions: raise or silently ignore.
    git-svn-id: http://prometheus.rubyforge.org/svn/scratch/ruby-nuggets@36 d799ba13-25ec-4c21-81e7-7bafc1e0b839

--- lib/nuggets/hash/insert.rb
+++ lib/nuggets/hash/insert.rb
@@ -7,8 +7,8 @@ class Hash
   # Inserts +value+ into _hash_ at +key+, while merging existing values at
   # +key+ instead of just overwriting. Uses default Hash#merge or block for
   # merging.
-  def insert(key, value, &block)
-    dup.insert!(key, value, &block)
+  def insert(key, value, relax = true, &block)
+    dup.insert!(key, value, relax, &block)
   end
 
   # call-seq:
@@ -16,13 +16,17 @@ class Hash
   #   hash.insert!(key, value) { |old_value, value| ... } => hash
   #
   # Destructive version of #insert.
-  def insert!(key, value, &block)
+  def insert!(key, value, relax = true, &block)
     block ||= lambda { |old_val, val| old_val.merge(val) }
 
     self[key] = begin
       block[self[key], value]
     rescue NoMethodError, TypeError
-      value
+      unless relax
+        raise err
+      else
+        value
+      end
     end
 
     self

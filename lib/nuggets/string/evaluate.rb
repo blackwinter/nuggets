76a9c188760456d2b5a287652eaf3587e4c9e15f
Jens Wille <jens.wille@uni-koeln.de>
2007-10-16 11:42:18 +0000

    <ruby-nuggets> lib/nuggets/string/msub.rb: Added String#msub -- "multiple substitution". lib/nuggets/string/evaluate.rb: Added String#evaluate -- allows to evaluate string substitution in different binding.
    git-svn-id: http://prometheus.rubyforge.org/svn/scratch/ruby-nuggets@103 d799ba13-25ec-4c21-81e7-7bafc1e0b839

---
+++ lib/nuggets/string/evaluate.rb
@@ -0,0 +1,53 @@
+#--
+###############################################################################
+#                                                                             #
+# A component of ruby-nuggets, some extensions to the Ruby programming        #
+# language.                                                                   #
+#                                                                             #
+# Copyright (C) 2007 Jens Wille                                               #
+#                                                                             #
+# Authors:                                                                    #
+#     Jens Wille <jens.wille@uni-koeln.de>                                    #
+#                                                                             #
+# ruby-nuggets is free software; you can redistribute it and/or modify it     #
+# under the terms of the GNU General Public License as published by the Free  #
+# Software Foundation; either version 3 of the License, or (at your option)   #
+# any later version.                                                          #
+#                                                                             #
+# ruby-nuggets is distributed in the hope that it will be useful, but WITHOUT #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    #
+# more details.                                                               #
+#                                                                             #
+# You should have received a copy of the GNU General Public License along     #
+# with ruby-nuggets. If not, see <http://www.gnu.org/licenses/>.              #
+#                                                                             #
+###############################################################################
+#++
+
+class String
+
+  # call-seq:
+  #   str.evaluate(binding = TOPLEVEL_BINDING) => new_str
+  #
+  # Basically turns Kernel#eval into an instance method of String -- inspired
+  # by Ruby Cookbook example 1.3. This allows to pre-populate strings with
+  # substitution expressions ("#{...}") that can get evaluated in a different
+  # environment (= +binding+) at a later point.
+  def evaluate(binding = TOPLEVEL_BINDING)
+    eval(%Q{"#{self}"}, binding)
+  end
+
+end
+
+if $0 == __FILE__
+  s = 'bl#{a}blub'
+  p s
+
+  def foo(bar)
+    a = 'ub'
+    bar.evaluate(binding)
+  end
+
+  p foo(s)
+end
---
+++ lib/nuggets/string/msub.rb
@@ -0,0 +1,82 @@
+#--
+###############################################################################
+#                                                                             #
+# A component of ruby-nuggets, some extensions to the Ruby programming        #
+# language.                                                                   #
+#                                                                             #
+# Copyright (C) 2007 Jens Wille                                               #
+#                                                                             #
+# Authors:                                                                    #
+#     Jens Wille <jens.wille@uni-koeln.de>                                    #
+#                                                                             #
+# ruby-nuggets is free software; you can redistribute it and/or modify it     #
+# under the terms of the GNU General Public License as published by the Free  #
+# Software Foundation; either version 3 of the License, or (at your option)   #
+# any later version.                                                          #
+#                                                                             #
+# ruby-nuggets is distributed in the hope that it will be useful, but WITHOUT #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    #
+# more details.                                                               #
+#                                                                             #
+# You should have received a copy of the GNU General Public License along     #
+# with ruby-nuggets. If not, see <http://www.gnu.org/licenses/>.              #
+#                                                                             #
+###############################################################################
+#++
+
+require File.join(File.dirname(__FILE__), 'evaluate')
+
+class String
+
+  # call-seq:
+  #   str.msub(*substitutions) => new_str
+  #
+  # Performs _multiple_ substitutions on _str_ with order being taken into
+  # account (thus results of previous substitutions won't be subject to later
+  # ones) -- inspired by Ruby Cookbook example 1.18.
+  #
+  # The +substitutions+ parameter can be an array or a list of <tt>[pattern,
+  # substitution]</tt> pairs, or, simply, a hash. Note that, when using a hash,
+  # the ordering of how substitutions are processed might differ from what you
+  # intended -- instead use an array when order matters. +pattern+ can be a
+  # string or a regexp, +substitution+ may contain string expressions (cf.
+  # #evaluate).
+  def msub(*substitutions)
+    dup.msub!(*substitutions) || dup
+  end
+
+  # call-seq:
+  #   str.msub!(*substitutions) => str or nil
+  #
+  # Destructive version of #msub.
+  def msub!(*substitutions)
+    substitutions = *substitutions  # Handle hashes and arrays alike
+    keys, subs, cache = [], [], {}
+
+    substitutions.each { |key, value|
+      key = Regexp.new(Regexp.escape(key)) unless key.is_a?(Regexp)
+
+      keys << key
+      subs << [key, value]
+    }
+
+    gsub!(Regexp.union(*keys)) { |match|
+      cache[match] ||= subs.find { |key,|
+        key =~ match
+      }.last.evaluate(binding)
+    }
+  end
+
+end
+
+if $0 == __FILE__
+  s = 'Foo, Bar - Baz'
+  p s
+
+  p s.gsub(/a/, 'o').gsub(/o/, 'a')
+  p s.msub('a' => 'o', 'o' => 'a')
+
+  s.msub!('a' => 'o','o' => 'a')
+  p s
+end

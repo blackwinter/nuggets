2476b59eebe2b3959496da50397c391422e3e5a0
Jens Wille <jens.wille@uni-koeln.de>
2008-01-11 11:03:16 +0000

    <ruby-nuggets> lib/nuggets/array/to_hash.rb: Array#to_hash now also takes a block instead of a value; extended documentation and examples. Added Array#to_hash_opt, a slightly speed-optimized, though less "nice", version of #to_hash.
    git-svn-id: http://prometheus.rubyforge.org/svn/scratch/ruby-nuggets@175 d799ba13-25ec-4c21-81e7-7bafc1e0b839

--- lib/nuggets/array/to_hash.rb
+++ lib/nuggets/array/to_hash.rb
@@ -4,7 +4,7 @@
 # A component of ruby-nuggets, some extensions to the Ruby programming        #
 # language.                                                                   #
 #                                                                             #
-# Copyright (C) 2007 Jens Wille                                               #
+# Copyright (C) 2007-2008 Jens Wille                                          #
 #                                                                             #
 # Authors:                                                                    #
 #     Jens Wille <jens.wille@uni-koeln.de>                                    #
@@ -32,24 +32,62 @@ class Array
   # call-seq:
   #   array.to_h => aHash
   #   array.to_h(value) => aHash
+  #   array.to_h { |element| ... } => aHash
   #
-  # If no +value+ is given, converts _array_, being an array of two-element
-  # arrays, into a hash, preserving sub-arrays. Otherwise, maps each element
-  # of _array_ to +value+.
+  # If neither +value+ nor block is given, converts _array_, taken as an
+  # array of key/value pairs, into a hash, preserving sub-arrays (Thus:
+  # <tt>hash.to_a.to_h == hash</tt>). Otherwise, maps each element of
+  # _array_ to +value+ or the result of the block.
+  #
+  # NOTE: This is the "nice" version. For a more speed-optimized one,
+  # use #to_hash_opt.
   #
   # Examples:
-  #   %w[a b c d].to_h     # => { "a" => "b", "c" => "d" }
-  #   %w[a b c d].to_h(1)  # => { "a" => 1, "b" => 1, "c" => 1, "d" => 1 }
-  def to_hash(value = default = Object.new)
-    if value == default
-      Hash[*flatten_once]
-    else
+  #   [[0, 0], [1, [2, 3]]].to_h  #=> { 0 => 0, 1 => [2, 3] }
+  #   %w[a b c d].to_h            #=> { "a" => "b", "c" => "d" }
+  #   %w[a b c d].to_h(1)         #=> { "a" => 1, "b" => 1, "c" => 1, "d" => 1 }
+  #   %w[a b].to_h { |e| e * 2 }  #=> { "a" => "aa", "b" => "bb" }
+  def to_hash(value = default = Object.new, &block)
+    if block ||= value != default && lambda { value }
       inject({}) { |hash, element|
-        hash.update(element => value)
+        hash.update(element => block[element])
       }
+    else
+      Hash[*flatten_once]
     end
   end
   alias_method :to_h, :to_hash
+
+  # call-seq:
+  #   array.to_h => aHash
+  #   array.to_h(value) => aHash
+  #   array.to_h { |element| ... } => aHash
+  #
+  # Same as #to_hash, but slightly optimized for speed. To use this one instead
+  # of #to_hash: <tt>class Array; alias_method :to_h, :to_hash_opt; end</tt>.
+  #
+  # Benchmark (array = (1..20).to_a, N = 100_000):
+  #                            user     system      total        real
+  #   to_hash: plain       4.820000   0.560000   5.380000 (  5.600390)
+  #   to_hash: value      12.910000   0.930000  13.840000 ( 13.938352)
+  #   to_hash: block      13.590000   1.180000  14.770000 ( 14.810804)
+  #   to_hash_opt: plain   4.910000   0.470000   5.380000 (  5.416949)
+  #   to_hash_opt: value   2.170000   0.390000   2.560000 (  2.609034)
+  #   to_hash_opt: block   7.090000   0.880000   7.970000 (  8.109180)
+  def to_hash_opt(value = default = Object.new, &block)
+    if block
+      hash = {}
+      each { |element| hash[element] = block[element] }
+      hash
+    elsif value != default
+      hash = {}
+      each { |element| hash[element] = value }
+      hash
+    else
+      Hash[*flatten_once]
+    end
+  end
+  #alias_method :to_h, :to_hash_opt
 
 end
 
@@ -64,5 +102,13 @@ if $0 == __FILE__
 
   c = %w[a b c d]
   p c
+  p c.to_h
   p c.to_h(1)
+  p c.to_h { nil }
+
+  h = { :a => 1, :b => [2, 3], :c => { :d => 4}}
+  p h
+  p h.to_a
+  p h.to_a.to_h
+  p h.to_a.to_h == h
 end

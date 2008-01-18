2ddfc51407f0ee4fac78a7a85f5260a23d6c9961
Jens Wille <jens.wille@uni-koeln.de>
2008-01-18 12:31:35 +0000

    <ruby-nuggets> lib/nuggets/integer/factorial.rb: Added Integer#factorial. lib/nuggets/version.rb: Increased version number.
    git-svn-id: http://prometheus.rubyforge.org/svn/scratch/ruby-nuggets@178 d799ba13-25ec-4c21-81e7-7bafc1e0b839

---
+++ lib/nuggets/integer/factorial.rb
@@ -0,0 +1,47 @@
+#--
+###############################################################################
+#                                                                             #
+# A component of ruby-nuggets, some extensions to the Ruby programming        #
+# language.                                                                   #
+#                                                                             #
+# Copyright (C) 2007-2008 Jens Wille                                          #
+#                                                                             #
+# Authors:                                                                    #
+#     Jens Wille <jens.wille@uni-koeln.de>                                    #
+#                                                                             #
+# ruby-nuggets is free software; you can redistribute it and/or modify it     #
+# under the terms of the GNU General Public License as published by the Free  #
+# Software Foundation; either version 3 of the License, or (at your option)   #
+# any later version.                                                          #
+#                                                                             #
+# ruby-nuggets is distributed in the hope that it will be useful, but WITHOUT #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    #
+# more details.                                                               #
+#                                                                             #
+# You should have received a copy of the GNU General Public License along     #
+# with ruby-nuggets. If not, see <http://www.gnu.org/licenses/>.              #
+#                                                                             #
+###############################################################################
+#++
+
+class Integer
+
+  # call-seq:
+  #   int.factorial => anInteger
+  #
+  # Calculate the factorial of +int+.
+  def factorial
+    (1..self).inject { |f, i| f * i }
+  end
+
+  alias_method :fac, :factorial
+  alias_method :f!,  :factorial
+
+end
+
+if $0 == __FILE__
+  1.upto(8) { |i|
+    puts "#{i}: #{i.factorial}"
+  }
+end

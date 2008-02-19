8269b5c1c39de6bb89ee7691b1fcc9858028d446
Jens Wille <jens.wille@uni-koeln.de>
2008-02-19 11:10:29 +0000

    <ruby-nuggets> lib/nuggets/hash/in_order.rb: Added Hash#in_order -- force order on hashes. lib/nuggets/version.rb: Increased version number.
    git-svn-id: http://prometheus.rubyforge.org/svn/scratch/ruby-nuggets@208 d799ba13-25ec-4c21-81e7-7bafc1e0b839

---
+++ lib/nuggets/hash/in_order.rb
@@ -0,0 +1,52 @@
+#--
+###############################################################################
+#                                                                             #
+# A component of ruby-nuggets, some extensions to the Ruby programming        #
+# language.                                                                   #
+#                                                                             #
+# Copyright (C) 2007-2008 Jens Wille                                          #
+#                                                                             #
+# Authors:                                                                    #
+#     Jens Wille <jens.wille@uni-koeln.de>                                    #
+#                                                                             #
+# ruby-nuggets is free software; you can redistribute it and/or modify it     #
+# under the terms of the GNU General Public License as published by the Free  #
+# Software Foundation; either version 3 of the License, or (at your option)   #
+# any later version.                                                          #
+#                                                                             #
+# ruby-nuggets is distributed in the hope that it will be useful, but WITHOUT #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    #
+# more details.                                                               #
+#                                                                             #
+# You should have received a copy of the GNU General Public License along     #
+# with ruby-nuggets. If not, see <http://www.gnu.org/licenses/>.              #
+#                                                                             #
+###############################################################################
+#++
+
+require File.join(File.dirname(__FILE__), '..', 'array', 'in_order')
+
+class Hash
+
+  # call-seq:
+  #   hash.in_order(*ordered) => anArray
+  #
+  # Returns <tt>hash#to_a</tt>, in forced order (cf. Array#in_order).
+  #
+  # Examples:
+  #   { :a => 1, :b => 2, :c => 3 }.in_order(:b, :c)  #=> [[:b, 2], [:c, 3], [:a, 1]]
+  #   { :a => 1, :b => 2, :c => 3 }.in_order(:b, :d)  #=> [[:b, 2], [:a, 1], [:c, 3]]
+  def in_order(*ordered)
+    keys.in_order(*ordered).map { |key| [key, self[key]] }
+  end
+
+end
+
+if $0 == __FILE__
+  a = { :a => 1, :b => 2, :c => 3 }
+  p a
+
+  p a.in_order(:b, :c)
+  p a.in_order(:b, :d)
+end

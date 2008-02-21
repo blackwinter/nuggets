c46bdddde7d635de6655613ca677928455c1ef80
Jens Wille <jens.wille@uni-koeln.de>
2008-02-21 09:44:12 +0000

    <ruby-nuggets> lib/nuggets/numeric/signum.rb: Added Numeric#signum -- Determine a numbers sign. lib/nuggets/version.rb: v0.1.1
    git-svn-id: http://prometheus.rubyforge.org/svn/scratch/ruby-nuggets@214 d799ba13-25ec-4c21-81e7-7bafc1e0b839

---
+++ lib/nuggets/numeric/signum.rb
@@ -0,0 +1,48 @@
+#--
+###############################################################################
+#                                                                             #
+# A component of ruby-nuggets, some extensions to the Ruby programming        #
+# language.                                                                   #
+#                                                                             #
+# Copyright (C) 2007-2008 Jens Wille                                          #
+#                                                                             #
+# Authors:                                                                    #
+#     Jens Wille <jens.wille@uni-koeln.de>                                    #
+#                                                                             #
+# ruby-nuggets is free software; you can redistribute it and/or modify it     #
+# under the terms of the GNU General Public License as published by the Free  #
+# Software Foundation; either version 3 of the License, or (at your option)   #
+# any later version.                                                          #
+#                                                                             #
+# ruby-nuggets is distributed in the hope that it will be useful, but WITHOUT #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    #
+# more details.                                                               #
+#                                                                             #
+# You should have received a copy of the GNU General Public License along     #
+# with ruby-nuggets. If not, see <http://www.gnu.org/licenses/>.              #
+#                                                                             #
+###############################################################################
+#++
+
+class Numeric
+
+  # call-seq:
+  #   num.signum => -1, 0, 1
+  #
+  # 
+  def signum
+    self > 0 ? 1 : self < 0 ? -1 : 0
+  end
+
+  alias_method :sign, :signum
+  alias_method :sgn,  :signum
+
+end
+
+if $0 == __FILE__
+  [123, -123, 0, 0.001, 1.23, -12.3].each { |n|
+    p n
+    p n.sgn
+  }
+end
--- lib/nuggets/version.rb
+++ lib/nuggets/version.rb
@@ -4,7 +4,7 @@ module Nuggets
 
     MAJOR = 0
     MINOR = 1
-    TINY  = 0
+    TINY  = 1
 
     class << self
 

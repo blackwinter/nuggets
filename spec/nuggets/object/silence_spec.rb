820207cc22ef861edec5cd2a222118709702115e
Jens Wille <jens.wille@uni-koeln.de>
2009-04-23 11:51:01 +0200

    spec/nuggets/object/silence_spec.rb: Fixes; useless use of context.

--- spec/nuggets/object/silence_spec.rb
+++ spec/nuggets/object/silence_spec.rb
@@ -4,34 +4,30 @@ describe Object, 'when extended by', Nuggets::Object::SilenceMixin do
 
   it { Object.ancestors.should include(Nuggets::Object::SilenceMixin) }
 
-  context do
+  before do
+    @stderr, @old_stderr = '', $stderr
+    $stderr = StringIO.new(@stderr)
 
-    before do
-      @stderr, @old_stderr = '', $stderr
-      $stderr = StringIO.new(@stderr)
+    AConst = :foo
+  end
 
-      AConst = :foo
-    end
+  after do
+    $stderr = @old_stderr
+    Object.send(:remove_const, :AConst) if Object.const_defined?(:AConst)
+  end
 
-    after do
-      $stderr = @old_stderr
-      Object.send(:remove_const, :AConst)
-    end
+  example do
+    AConst = :bar
 
-    example do
-      AConst = :bar
+    AConst.should be_equal(:bar)
+    @stderr.should =~ /warning: already initialized constant AConst/
+  end
 
-      AConst.should be_equal(:bar)
-      @stderr.should =~ /warning: already initialized constant AConst/
-    end
+  example do
+    silence { AConst = :baz }
 
-    example do
-      silence { AConst = :bar }
-
-      AConst.should be_equal(:bar)
-      @stderr.should == ''
-    end
-
+    AConst.should be_equal(:baz)
+    @stderr.should == ''
   end
 
 end

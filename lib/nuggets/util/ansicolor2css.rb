5cbbc529a3461f6d657df7f3ddd26c834f9d5538
Jens Wille <jens.wille@uni-koeln.de>
2010-06-28 16:15:48 +0200

    v0.6.3

--- lib/nuggets/util/ansicolor2css.rb
+++ lib/nuggets/util/ansicolor2css.rb
@@ -4,7 +4,7 @@
 # A component of ruby-nuggets, some extensions to the Ruby programming        #
 # language.                                                                   #
 #                                                                             #
-# Copyright (C) 2007-2008 Jens Wille                                          #
+# Copyright (C) 2007-2010 Jens Wille                                          #
 #                                                                             #
 # Authors:                                                                    #
 #     Jens Wille <jens.wille@uni-koeln.de>                                    #
--- lib/nuggets/version.rb
+++ lib/nuggets/version.rb
@@ -4,7 +4,7 @@ module Nuggets
 
     MAJOR = 0
     MINOR = 6
-    TINY  = 2
+    TINY  = 3
 
     class << self
 

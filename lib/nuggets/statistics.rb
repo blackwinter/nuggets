a15fea9e232338fd2386f8e534cb31c83a6fb9a0
Jens Wille <jens.wille@uni-koeln.de>
2011-04-05 16:39:58 +0200

    Allow to load all statistics related nuggets at once.

---
+++ lib/nuggets/statistics.rb
@@ -0,0 +1,11 @@
+# load statistics related nuggets
+
+require 'nuggets/array/correlation'
+require 'nuggets/array/histogram'
+require 'nuggets/array/mean'
+require 'nuggets/array/median'
+require 'nuggets/array/mode'
+require 'nuggets/array/regression'
+require 'nuggets/array/standard_deviation'
+require 'nuggets/array/variance'
+require 'nuggets/range/quantile'
---
+++ lib/nuggets/statistics_mixins.rb
@@ -0,0 +1,11 @@
+# load statistics related nuggets (mixins)
+
+require 'nuggets/array/correlation_mixin'
+require 'nuggets/array/histogram_mixin'
+require 'nuggets/array/mean_mixin'
+require 'nuggets/array/median_mixin'
+require 'nuggets/array/mode_mixin'
+require 'nuggets/array/regression_mixin'
+require 'nuggets/array/standard_deviation_mixin'
+require 'nuggets/array/variance_mixin'
+require 'nuggets/range/quantile_mixin'

8c0b2da7f8643967416087a2102c6bcc48b1cb00
Jens Wille <jens.wille@uni-koeln.de>
2011-04-29 15:07:12 +0200

    Switched license to Affero GPL.

--- lib/nuggets/env/set_mixin.rb
+++ lib/nuggets/env/set_mixin.rb
@@ -4,23 +4,23 @@
 # A component of ruby-nuggets, some extensions to the Ruby programming        #
 # language.                                                                   #
 #                                                                             #
-# Copyright (C) 2008-2009 Jens Wille                                          #
+# Copyright (C) 2007-2011 Jens Wille                                          #
 #                                                                             #
 # Authors:                                                                    #
 #     Jens Wille <jens.wille@uni-koeln.de>                                    #
 #                                                                             #
 # ruby-nuggets is free software; you can redistribute it and/or modify it     #
-# under the terms of the GNU General Public License as published by the Free  #
-# Software Foundation; either version 3 of the License, or (at your option)   #
-# any later version.                                                          #
+# under the terms of the GNU Affero General Public License as published by    #
+# the Free Software Foundation; either version 3 of the License, or (at your  #
+# option) any later version.                                                  #
 #                                                                             #
 # ruby-nuggets is distributed in the hope that it will be useful, but WITHOUT #
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       #
-# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for    #
-# more details.                                                               #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License #
+# for more details.                                                           #
 #                                                                             #
-# You should have received a copy of the GNU General Public License along     #
-# with ruby-nuggets. If not, see <http://www.gnu.org/licenses/>.              #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with ruby-nuggets. If not, see <http://www.gnu.org/licenses/>.        #
 #                                                                             #
 ###############################################################################
 #++

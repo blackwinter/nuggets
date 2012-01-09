3adbae6a878a0d9dc675da51730f89a5cba2e6d9
Jens Wille <jens.wille@uni-koeln.de>
2012-01-09 16:55:33 +0100

    Added IO#redirect -- Temporarily redirect IO somewhere else.

---
+++ lib/nuggets/io/redirect.rb
@@ -0,0 +1,5 @@
+require 'nuggets/io/redirect_mixin'
+
+class IO
+  include Nuggets::IO::RedirectMixin
+end
---
+++ lib/nuggets/io/redirect_mixin.rb
@@ -0,0 +1,51 @@
+#--
+###############################################################################
+#                                                                             #
+# A component of ruby-nuggets, some extensions to the Ruby programming        #
+# language.                                                                   #
+#                                                                             #
+# Copyright (C) 2007-2012 Jens Wille                                          #
+#                                                                             #
+# Authors:                                                                    #
+#     Jens Wille <jens.wille@uni-koeln.de>                                    #
+#                                                                             #
+# ruby-nuggets is free software; you can redistribute it and/or modify it     #
+# under the terms of the GNU Affero General Public License as published by    #
+# the Free Software Foundation; either version 3 of the License, or (at your  #
+# option) any later version.                                                  #
+#                                                                             #
+# ruby-nuggets is distributed in the hope that it will be useful, but WITHOUT #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License #
+# for more details.                                                           #
+#                                                                             #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with ruby-nuggets. If not, see <http://www.gnu.org/licenses/>.        #
+#                                                                             #
+###############################################################################
+#++
+
+require 'nuggets/io/null_mixin'
+
+module Nuggets
+  class IO
+    module RedirectMixin
+
+  def redirect(target = NullMixin::NULL, mode = 'w')
+    unless target.is_a?(self.class)
+      target = ::File.open(target, mode)
+      close_target = true
+    end
+
+    prev = dup
+    reopen(target)
+
+    yield
+  ensure
+    reopen(prev) if prev
+    target.close if close_target
+  end
+
+    end
+  end
+end

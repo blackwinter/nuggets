d78d48fa72e2dde74ba7b4961120d07ba152b454
Jens Wille <jens.wille@uni-koeln.de>
2012-01-27 17:05:55 +0100

    Added String#{camelcase,underscore,constantize} (based on ActiveSupport).

---
+++ lib/nuggets/string/camelscore_mixin.rb
@@ -0,0 +1,117 @@
+#--
+###############################################################################
+#                                                                             #
+# A component of ruby-nuggets, some extensions to the Ruby programming        #
+# language.                                                                   #
+#                                                                             #
+# Copyright (C) 2007-2012 Jens Wille                                          #
+#                                                                             #
+# Authors:                                                                    #
+#     Jens Wille <jens.wille@uni-koeln.de>                                    #
+#                                                                             #
+# ruby-nuggets is free software; you can redistribute it and/or modify it     #
+# under the terms of the GNU Affero General Public License as published by    #
+# the Free Software Foundation; either version 3 of the License, or (at your  #
+# option) any later version.                                                  #
+#                                                                             #
+# ruby-nuggets is distributed in the hope that it will be useful, but WITHOUT #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License #
+# for more details.                                                           #
+#                                                                             #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with ruby-nuggets. If not, see <http://www.gnu.org/licenses/>.        #
+#                                                                             #
+###############################################################################
+#++
+
+module Nuggets
+  class String
+    module CamelscoreMixin
+
+  # List of acronyms to treat specially
+  # in #camelcase and #underscore.
+  CAMELSCORE_ACRONYMS = {
+    'html' => 'HTML',
+    'rss'  => 'RSS',
+    'sql'  => 'SQL',
+    'ssl'  => 'SSL',
+    'xml'  => 'XML'
+  }
+
+  # call-seq:
+  #   str.camelcase => new_string
+  #
+  # Returns the CamelCase form of _str_.
+  def camelcase
+    dup.camelcase!
+  end
+
+  alias_method :camelize, :camelcase
+
+  # call-seq:
+  #   str.camelcase! => str
+  #
+  # Replaces _str_ with its CamelCase form and returns _str_.
+  def camelcase!
+    sub!(/^[a-z]+/) {
+      CAMELSCORE_ACRONYMS[$&] || $&.capitalize
+    }
+
+    gsub!(/(?:_|([\/\d]))([a-z]+)/i) {
+      "#{$1}#{CAMELSCORE_ACRONYMS[$2] || $2.capitalize}"
+    }
+
+    gsub!('/', '::')
+
+    self
+  end
+
+  alias_method :camelize!, :camelcase!
+
+  # call-seq:
+  #   str.underscore => new_string
+  #
+  # Returns the under_score form of _str_.
+  def underscore
+    dup.underscore!
+  end
+
+  # call-seq:
+  #   str.underscore! => str
+  #
+  # Replaces _str_ with its under_score form and returns _str_.
+  def underscore!
+    gsub!(/::/, '/')
+
+    a = CAMELSCORE_ACRONYMS.values
+    r = a.empty? ? /(?=a)b/ : Regexp.union(*a)
+
+    gsub!(/(?:([A-Za-z])|(\d)|^)(#{r})(?=\b|[^a-z])/) {
+      "#{$1 || $2}#{'_' if $1}#{$3.downcase}"
+    }
+
+    gsub!(/([A-Z])(?=[A-Z])/, '\1_')
+    gsub!(/([a-z\d])([A-Z])/, '\1_\2')
+
+    downcase!
+
+    self
+  end
+
+  # call-seq:
+  #   str.constantize(base = Object) => anObject
+  #
+  # Returns the constant pointed to by _str_, relative to +base+.
+  def constantize(base = Object)
+    names = split('::')
+    return if names.empty?
+
+    const = names.first.empty? ? (names.shift; Object) : base
+    names.each { |name| const = const.const_get(name) }
+    const
+  end
+
+    end
+  end
+end
---
+++ spec/nuggets/string/camelscore_spec.rb
@@ -0,0 +1,116 @@
+require 'nuggets/string/camelscore'
+
+describe String, 'when extended by', Nuggets::String::CamelscoreMixin do
+
+  it { String.ancestors.should include(Nuggets::String::CamelscoreMixin) }
+
+  def self.with_acronyms(acronyms, &block)
+    describe "with acronyms #{acronyms.inspect}" do
+      before :all do String::CAMELSCORE_ACRONYMS.replace(acronyms) end
+      after  :all do String::CAMELSCORE_ACRONYMS.clear             end
+
+      instance_eval(&block)
+    end
+  end
+
+  with_acronyms({}) { [
+    ['', ''],
+    %w[a A],
+    %w[a_b AB],
+    %w[a_b_c ABC],
+    %w[a/b A::B],
+    %w[a/b_c A::BC],
+    %w[a/b/c A::B::C],
+    %w[foo Foo],
+    %w[foo_bar FooBar],
+    %w[foo_bar_baz FooBarBaz],
+    %w[foo/bar Foo::Bar],
+    %w[foo/bar_baz Foo::BarBaz],
+    %w[foo/bar/baz Foo::Bar::Baz],
+    %w[active_model ActiveModel],
+    %w[active_model/errors ActiveModel::Errors],
+    %w[ssl_error SslError],
+    %w[s_s_l_error SSLError]
+  ].each { |a, b|
+    example { a.camelcase.should == b }
+    example { b.underscore.should == a }
+  } }
+
+  with_acronyms('ab' => 'AB') {
+    example { 'ab'.camelcase.should == 'AB' }
+    example { 'AB'.underscore.should == 'ab' }
+    example { 'abc'.camelcase.should == 'Abc' }
+    example { 'Abc'.underscore.should == 'abc' }
+    example { 'ab_c'.camelcase.should == 'ABC' }
+    example { 'ABC'.underscore.should == 'ab_c' }
+  }
+
+  with_acronyms('abc' => 'ABC') {
+    example { 'ab'.camelcase.should == 'Ab' }
+    example { 'AB'.underscore.should == 'a_b' }
+    example { 'abc'.camelcase.should == 'ABC' }
+    example { 'ABC'.underscore.should == 'abc' }
+  }
+
+  with_acronyms('ab' => 'AB', 'abc' => 'ABC') {
+    example { 'ab'.camelcase.should == 'AB' }
+    example { 'AB'.underscore.should == 'ab' }
+    example { 'abc'.camelcase.should == 'ABC' }
+    example { 'Abc'.underscore.should == 'abc' }
+    example { 'ab_c'.camelcase.should == 'ABC' }
+    example { 'ABC'.underscore.should == 'ab_c' }
+  }
+
+  with_acronyms('xml' => 'XML', 'html' => 'HTML', 'sql' => 'SQL') {
+    example { 'xml'.camelcase.should == 'XML' }
+    example { 'XML'.underscore.should == 'xml' }
+    example { 'html'.camelcase.should == 'HTML' }
+    example { 'HTML'.underscore.should == 'html' }
+    example { 'html5'.camelcase.should == 'HTML5' }
+    example { 'HTML5'.underscore.should == 'html5' }
+    pending { 'xhtml'.camelcase.should == 'XHTML' }
+    pending { 'XHTML'.underscore.should == 'xhtml' }
+    example { 'sql'.camelcase.should == 'SQL' }
+    example { 'SQL'.underscore.should == 'sql' }
+    pending { 'mysql'.camelcase.should == 'MySQL' }
+    pending { 'MySQL'.underscore.should == 'mysql' }
+    pending { 'postgresql'.camelcase.should == 'PostgreSQL' }
+    pending { 'PostgreSQL'.underscore.should == 'postgresql' }
+    pending { 'sqlite'.camelcase.should == 'SQLite' }
+    pending { 'SQLite'.underscore.should == 'sqlite' }
+    pending { 'nosql'.camelcase.should == 'NoSQL' }
+    pending { 'NoSQL'.underscore.should == 'nosql' }
+  }
+
+  with_acronyms('rss' => 'RSS', 'xml' => 'XML') {
+    example { 'rss2xml'.camelcase.should == 'RSS2XML' }
+    example { 'RSS2XML'.underscore.should == 'rss2xml' }
+  }
+
+  with_acronyms('ssl' => 'SSL') {
+    example { 'ssl'.camelcase.should == 'SSL' }
+    example { 'SSL'.underscore.should == 'ssl' }
+    example { 'ssl_error'.camelcase.should == 'SSLError' }
+    example { 'SSLError'.underscore.should == 'ssl_error' }
+  }
+
+  example { ''.constantize.should be_nil }
+  example { '::'.constantize.should be_nil }
+
+  example { 'Nuggets'.constantize.should == ::Nuggets }
+  example { '::Nuggets'.constantize.should == ::Nuggets }
+
+  example { 'Nuggets::String'.constantize.should == ::Nuggets::String }
+  example { '::Nuggets::String'.constantize.should == ::Nuggets::String }
+
+  example { 'String'.constantize.should == ::String }
+  example { 'String'.constantize(::Nuggets).should == ::Nuggets::String }
+  example { '::String'.constantize(::Nuggets).should == ::String }
+  example { 'String'.constantize(::Nuggets::String).should == ::String }
+  example { 'String'.constantize(::Nuggets::String::CamelscoreMixin).should == ::String }
+
+  example { lambda { 'CamelscoreMixin'.constantize }.should raise_error(NameError) }
+  example { lambda { 'CamelscoreMixin'.constantize(::Nuggets) }.should raise_error(NameError) }
+  example { 'CamelscoreMixin'.constantize(::Nuggets::String).should == ::Nuggets::String::CamelscoreMixin }
+
+end

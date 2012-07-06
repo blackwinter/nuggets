3a3cf0fc4794c524cceecc25e026ebd7c0539188
Jens Wille <jens.wille@uni-koeln.de>
2012-07-06 17:24:00 +0200

    Added parsers for Apache and Rails log files.

---
+++ lib/nuggets/util/log_parser/apache.rb
@@ -0,0 +1,106 @@
+#--
+###############################################################################
+#                                                                             #
+# A component of ruby-nuggets, some extensions to the Ruby programming        #
+# language.                                                                   #
+#                                                                             #
+# Copyright (C) 2007-2012 Jens Wille                                          #
+#                                                                             #
+# Authors:                                                                    #
+#     Jens Wille <jens.wille@uni-koeln.de>                                    #
+#                                                                             #
+# ruby-nuggets is free software; you can redistribute it and/or modify it     #
+# under the terms of the GNU Affero General Public License as published by    #
+# the Free Software Foundation; either version 3 of the License, or (at your  #
+# option) any later version.                                                  #
+#                                                                             #
+# ruby-nuggets is distributed in the hope that it will be useful, but WITHOUT #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License #
+# for more details.                                                           #
+#                                                                             #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with ruby-nuggets. If not, see <http://www.gnu.org/licenses/>.        #
+#                                                                             #
+###############################################################################
+#++
+
+require 'nuggets/util/log_parser'
+
+module Util
+
+  module LogParser
+
+    module Apache
+
+      extend self
+
+      DEFAULT_RE = %r{(.*?)}
+
+      DIRECTIVES = {
+        'h' => [:ip,        %r{(\d+(?:\.\d+){3}|[\w.-]+)}],
+        'l' => [:auth,      DEFAULT_RE],
+        'u' => [:username,  DEFAULT_RE],
+        't' => [:datetime,  %r{\[(.*?)\]}],
+        'r' => [:request,   DEFAULT_RE],
+        'R' => [:request,   %r{(.*?)(?:"|\z)}],
+        's' => [:status,    %r{(\d+)}],
+        'b' => [:bytecount, %r{(-|\d+)}],
+        'v' => [:domain,    DEFAULT_RE],
+        'i' => [nil,        DEFAULT_RE],
+      }
+
+      DIRECTIVES_RE = %r{%.*?(?:\{(.*?)\})?([#{DIRECTIVES.keys.join}])([\s\\"]*)}
+
+      ORDER = []
+
+      class << self
+
+        def register(name, format)
+          base = const_set(name, Module.new)
+
+          re, items = parse_format(format)
+          base.const_set(:RE,    re)
+          base.const_set(:ITEMS, items)
+
+          ORDER << base
+          LogParser.register(base, self)
+        end
+
+        def parse_format(format)
+          re, items = '\A', []
+
+          format.scan(DIRECTIVES_RE) { |h, c, e|
+            i, r = DIRECTIVES[c]
+            re << r.source << e.gsub(/\s/, '\\s')
+            items << i ||= h.downcase.tr('-', '_').to_sym
+          }
+
+          [Regexp.new(re), items]
+        end
+
+        def detect_type(line)
+          ORDER.find { |type| line =~ type::RE }
+        end
+
+      end
+
+      [ [:Combined, '%h %l %u %t "%r" %>s %b "%{Referer}i" "%{User-agent}i"'],
+        [:Common,   '%h %l %u %t "%r" %>s %b'],
+        [:Minimal,  '%h %l %u %t "%R']
+      ].each { |name, format| register(name, format) }
+
+      def parse_line(line, entry = {})
+        if md = self::RE.match(line)
+          self::ITEMS.each_with_index { |k, i| entry[k] = md[i + 1] }
+          yield if block_given?
+        end
+
+        entry
+      end
+
+    end
+
+  end
+
+end
---
+++ lib/nuggets/util/log_parser/rails.rb
@@ -0,0 +1,224 @@
+#--
+###############################################################################
+#                                                                             #
+# A component of ruby-nuggets, some extensions to the Ruby programming        #
+# language.                                                                   #
+#                                                                             #
+# Copyright (C) 2007-2012 Jens Wille                                          #
+#                                                                             #
+# Authors:                                                                    #
+#     Jens Wille <jens.wille@uni-koeln.de>                                    #
+#                                                                             #
+# ruby-nuggets is free software; you can redistribute it and/or modify it     #
+# under the terms of the GNU Affero General Public License as published by    #
+# the Free Software Foundation; either version 3 of the License, or (at your  #
+# option) any later version.                                                  #
+#                                                                             #
+# ruby-nuggets is distributed in the hope that it will be useful, but WITHOUT #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License #
+# for more details.                                                           #
+#                                                                             #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with ruby-nuggets. If not, see <http://www.gnu.org/licenses/>.        #
+#                                                                             #
+###############################################################################
+#++
+
+require 'nuggets/util/log_parser'
+
+module Util
+
+  module LogParser
+
+    module Rails
+
+      LogParser.register(self)
+
+      # Log line prefix
+      PREFIX_RE = %r{
+        \A
+        (?:
+          \[
+            (\d+)                #  pid
+            :
+            (.*?)                #  host
+          \]
+          \s
+        )?
+        \s*
+      }x
+
+      # Log entry separator
+      SEPARATOR_RE = %r{
+        \s+\|\s+
+      }x
+
+      # Log line patterns
+      ITEMS = [
+        [:processing, {
+          :re => %r{
+            #{PREFIX_RE}         #  pid, host
+            Processing\s+
+            (\w+)                #  controller
+            \#
+            (\w+)                #  action
+            \s+
+            \(
+              for\s+
+              (.*?)              #  ip
+              \s+at\s+
+              (.*?)              #  datetime
+            \)
+            \s+
+            \[
+              (\w+)              #  request_method
+            \]
+          }xo,
+          :keys => [:controller, :action, :ip, :datetime, :request_method]
+        }],
+        [:session_id, {
+          :re => %r{
+            #{PREFIX_RE}         #  pid, host
+            Session\sID:\s+
+            (\S+)                #  sid
+          }xo,
+          :keys => [:sid]
+        }],
+        [:parameters, {
+          :re => %r{
+            #{PREFIX_RE}         #  pid, host
+            Parameters:\s+
+            (\{.*\})             #  params
+          }xo,  #}
+          :proc => lambda { |entry, md|
+            entry[:params_hash] = md[3].hash
+            entry[:params] = begin
+              eval("$SAFE = 3\n#{md[3].gsub(/#<.*?>/, '%q{\&}')}", nil, __FILE__, __LINE__)  # !!!
+            rescue SyntaxError, SecurityError
+              {}
+            end
+          }
+        }],
+        [:client_info, {
+          :re => %r{
+            #{PREFIX_RE}         #  pid, host
+            Client\sinfo:\s+
+            UA\s+=\s+
+            (.*?)                #  user_agent
+            #{SEPARATOR_RE}
+            LANG\s+=\s+
+            (.*)                 #  accept_language
+          }xo,
+          :keys => [:user_agent, :accept_language]
+        }],
+        [:referer, {
+          :re => %r{
+            #{PREFIX_RE}         #  pid, host
+            Referer:\s+
+            (.*)                 #  referer
+          }xo,
+          :keys => [:referer]
+        }],
+        [:meta, {
+          :re => %r{
+            #{PREFIX_RE}         #  pid, host
+            Meta:\s+
+            User\s+=\s+
+            (.*?)                #  user_id
+            #{SEPARATOR_RE}
+            Institution\s+=\s+
+            (.*)                 #  institution_id
+          }xo,
+          :keys => [:user_id, :institution_id]
+        }],
+        [:stats, {
+          :re => %r{
+            #{PREFIX_RE}         #  pid, host
+            Stats:\s+
+            (.*)                 # flags
+          }xo,
+          :proc => lambda { |entry, md|
+            entry[:flags] = md[3].split(SEPARATOR_RE)
+          }
+        }],
+        [:oauth, {
+          :re => %r{
+            #{PREFIX_RE}         #  pid, host
+            OAuth:\s+
+            Token\s+=\s+
+            (.*?)#(.*?)          #  token_type, token
+            #{SEPARATOR_RE}
+            User\s+=\s+
+            (.*?)                #  user_id
+            #{SEPARATOR_RE}
+            Client\s+=\s+
+            (.*)                 #  client_id
+          }xo,
+          :keys => [:token_type, :token, :user_id, :client_id]
+        }],
+        [:benchmark, {
+          :re => %r{
+            #{PREFIX_RE}         #  pid, host
+            Completed\sin\s+
+            (\S+)                #  runtime
+            .*?
+            (?:                  #- OPTIONAL
+              #{SEPARATOR_RE}
+              Rendering:\s+
+              (\S+)              #  rendering_runtime
+              .*?
+            )?
+            (?:                  #- OPTIONAL
+              #{SEPARATOR_RE}
+              DB:\s+
+              (\S+)              #  db_runtime
+              .*?
+            )?
+            (?:                  #- OPTIONAL
+              #{SEPARATOR_RE}
+              Mem:\s+
+              \S+
+              .*?
+            )?
+            #{SEPARATOR_RE}
+            (.*?)                #  status
+            \s+
+            \[
+              (.*)               #  request_uri
+            \]
+          }xo,
+          :keys => [:runtime, :rendering_runtime, :db_runtime, :status, :request_uri]
+        }]
+      ]
+
+      ITEMS.each { |_, item|
+        item[:proc] ||= lambda { |entry, md|
+          item[:keys].each_with_index { |k, i|
+            entry[k] = md[i + 3]  # 1 = pid, 2 = host
+          }
+        }
+      }
+
+      def parse_line(line, entry = {})
+        ITEMS.each { |key, item|
+          if md = item[:re].match(line)
+            if key == :processing
+              yield if block_given?
+              entry[:pid], entry[:host] = md[1], md[2]
+            end
+
+            item[:proc][entry, md]
+
+            break
+          end
+        }
+
+        entry
+      end
+
+    end
+
+  end
+
+end

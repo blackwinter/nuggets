86a5aee0b970375c4440168ad540e6898c39c6eb
Jens Wille <jens.wille@gmail.com>
2013-03-25 16:45:49 +0100

    lib/nuggets/lazy_attr.rb: Moved from Util namespace.

--- lib/nuggets/util/lazy_attr.rb
+++ lib/nuggets/util/lazy_attr.rb
@@ -1,45 +1,4 @@
-#--
-###############################################################################
-#                                                                             #
-# A component of ruby-nuggets, some extensions to the Ruby programming        #
-# language.                                                                   #
-#                                                                             #
-# Copyright (C) 2007-2012 Jens Wille                                          #
-#                                                                             #
-# Authors:                                                                    #
-#     Jens Wille <jens.wille@gmail.com>                                       #
-#                                                                             #
-# ruby-nuggets is free software; you can redistribute it and/or modify it     #
-# under the terms of the GNU Affero General Public License as published by    #
-# the Free Software Foundation; either version 3 of the License, or (at your  #
-# option) any later version.                                                  #
-#                                                                             #
-# ruby-nuggets is distributed in the hope that it will be useful, but WITHOUT #
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       #
-# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License #
-# for more details.                                                           #
-#                                                                             #
-# You should have received a copy of the GNU Affero General Public License    #
-# along with ruby-nuggets. If not, see <http://www.gnu.org/licenses/>.        #
-#                                                                             #
-###############################################################################
-#++
+require 'nuggets/lazy_attr'
+module Util; LazyAttr = ::Nuggets::LazyAttr; end
 
-module Util
-
-  module LazyAttr
-
-    private
-
-    def lazy_attr(attr, freeze = true)
-      class << self; self; end.class_eval { attr_reader attr }
-
-      value = instance_variable_get(name = "@#{attr}") ||
-              instance_variable_set(name, yield)
-
-      freeze ? value.freeze : value
-    end
-
-  end
-
-end
+warn "#{__FILE__}: Util::LazyAttr is deprecated, use Nuggets::LazyAttr instead."

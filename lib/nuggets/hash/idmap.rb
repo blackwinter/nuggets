2cef8c6c7ec81c83af7f8da4632c0c9acc0b0bb7
Jens Wille <jens.wille@gmail.com>
2013-07-24 17:10:00 +0200

    Added Hash.idmap (every key gets a unique ID).

---
+++ lib/nuggets/hash/idmap.rb
@@ -0,0 +1,5 @@
+require 'nuggets/hash/idmap_mixin'
+
+class Hash
+  extend Nuggets::Hash::IDMapMixin
+end
---
+++ lib/nuggets/hash/idmap_mixin.rb
@@ -0,0 +1,42 @@
+#--
+###############################################################################
+#                                                                             #
+# A component of ruby-nuggets, some extensions to the Ruby programming        #
+# language.                                                                   #
+#                                                                             #
+# Copyright (C) 2007-2013 Jens Wille                                          #
+#                                                                             #
+# Authors:                                                                    #
+#     Jens Wille <jens.wille@gmail.com>                                       #
+#                                                                             #
+# ruby-nuggets is free software; you can redistribute it and/or modify it     #
+# under the terms of the GNU Affero General Public License as published by    #
+# the Free Software Foundation; either version 3 of the License, or (at your  #
+# option) any later version.                                                  #
+#                                                                             #
+# ruby-nuggets is distributed in the hope that it will be useful, but WITHOUT #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License #
+# for more details.                                                           #
+#                                                                             #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with ruby-nuggets. If not, see <http://www.gnu.org/licenses/>.        #
+#                                                                             #
+###############################################################################
+#++
+
+module Nuggets
+  class Hash
+    module IDMapMixin
+
+  def idmap(counter = 0)
+    if block_given?
+      new { |hash, key| hash[key] = yield(counter += 1) }
+    else
+      new { |hash, key| hash[key] = counter += 1 }
+    end
+  end
+
+    end
+  end
+end
